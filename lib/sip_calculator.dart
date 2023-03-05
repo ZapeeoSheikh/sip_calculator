0aabc5211a8aad99589d3e27b7c437eef0100c12
Muhammad Rameez <muhammadrmz871@gmail.com>
2023-03-05 13:56:20 +0500

    completed

--- lib/sip_calculator.dart
+++ lib/sip_calculator.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:sip_calculator/ui%20kit/ui.dart';
 
@@ -9,6 +10,16 @@ class SipCal extends StatefulWidget {
 }
 
 class _SipCalState extends State<SipCal> {
+  final initialController = TextEditingController();
+  final rateOfReturnController = TextEditingController();
+  final durationController = TextEditingController();
+
+  int totalAmount = 0;
+  int initialAmount = 0;
+  int profitAmount = 0;
+  String muturityAmount = "";
+  int rateOdReturn = 0;
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -36,108 +47,224 @@ class _SipCalState extends State<SipCal> {
                       Row(
                         children: [
                           Container(
-                            width: MediaQuery.of(context).size.width/3,
+                            width: MediaQuery.of(context).size.width / 3,
                             child: Text(
                               "Initial Investment Amount",
-                              style: TextStyle(color: MyColor.mainColor1, fontSize: 16, fontWeight: FontWeight.bold),
+                              style: TextStyle(
+                                  color: MyColor.mainColor1,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold),
                             ),
                           ),
-                          SizedBox(width: 40,),
+                          SizedBox(
+                            width: 30,
+                          ),
                           Expanded(
                             child: TextField(
+                              controller: initialController,
+                              keyboardType: TextInputType.number,
                               cursorColor: MyColor.mainColor1,
                               decoration: InputDecoration(
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(20)
-                                )
-                              ),
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(20))),
                             ),
                           )
                         ],
                       ),
-                      SizedBox(height: 40,),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      // Row(
+                      //   children: [
+                      //     Container(
+                      //       width: MediaQuery.of(context).size.width/3,
+                      //       child: Text(
+                      //         "SWP Amount",
+                      //         style: TextStyle(color: MyColor.mainColor1, fontSize: 16, fontWeight: FontWeight.bold),
+                      //       ),
+                      //     ),
+                      //     SizedBox(width: 30,),
+                      //     Expanded(
+                      //       child: TextField(
+                      //         cursorColor: MyColor.mainColor1,
+                      //         decoration: InputDecoration(
+                      //           border: OutlineInputBorder(
+                      //             borderRadius: BorderRadius.circular(20)
+                      //           )
+                      //         ),
+                      //       ),
+                      //     )
+                      //   ],
+                      // ),
+                      // SizedBox(height: 30,),
                       Row(
                         children: [
                           Container(
-                            width: MediaQuery.of(context).size.width/3,
+                            width: MediaQuery.of(context).size.width / 3,
                             child: Text(
-                              "Initial Investment Amount",
-                              style: TextStyle(color: MyColor.mainColor1, fontSize: 16, fontWeight: FontWeight.bold),
+                              "Rate of return (%)",
+                              style: TextStyle(
+                                  color: MyColor.mainColor1,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold),
                             ),
                           ),
-                          SizedBox(width: 40,),
+                          SizedBox(
+                            width: 30,
+                          ),
                           Expanded(
                             child: TextField(
+                              controller: rateOfReturnController,
+                              keyboardType: TextInputType.number,
                               cursorColor: MyColor.mainColor1,
                               decoration: InputDecoration(
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(20)
-                                )
-                              ),
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(20))),
                             ),
                           )
                         ],
                       ),
-                      SizedBox(height: 40,),
+                      SizedBox(
+                        height: 30,
+                      ),
                       Row(
                         children: [
                           Container(
-                            width: MediaQuery.of(context).size.width/3,
+                            width: MediaQuery.of(context).size.width / 3,
                             child: Text(
-                              "Initial Investment Amount",
-                              style: TextStyle(color: MyColor.mainColor1, fontSize: 16, fontWeight: FontWeight.bold),
+                              "Duration",
+                              style: TextStyle(
+                                  color: MyColor.mainColor1,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold),
                             ),
                           ),
-                          SizedBox(width: 40,),
+                          SizedBox(
+                            width: 30,
+                          ),
                           Expanded(
                             child: TextField(
+                              controller: durationController,
+                              keyboardType: TextInputType.number,
                               cursorColor: MyColor.mainColor1,
                               decoration: InputDecoration(
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(20)
-                                )
-                              ),
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(20))),
                             ),
                           )
                         ],
                       ),
-                      SizedBox(height: 40,),
-                      Row(
-                        children: [
-                          Container(
-                            width: MediaQuery.of(context).size.width/3,
-                            child: Text(
-                              "Initial Investment Amount",
-                              style: TextStyle(color: MyColor.mainColor1, fontSize: 16, fontWeight: FontWeight.bold),
-                            ),
-                          ),
-                          SizedBox(width: 40,),
-                          Expanded(
-                            child: TextField(
-                              cursorColor: MyColor.mainColor1,
-                              decoration: InputDecoration(
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(20)
-                                )
-                              ),
-                            ),
-                          )
-                        ],
+                      SizedBox(
+                        height: 40,
                       ),
-                      SizedBox(height: 40,),
 
                       Row(
                         children: [
                           Expanded(
-                            child: ElevatedButton(
-                                style: ElevatedButton.styleFrom(
-                                  backgroundColor: MyColor.mainColor1
-                                ),
+                            child: Container(
+                              height: 50,
+                              child: ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                      backgroundColor: MyColor.mainColor1),
+                                  onPressed: () {
+                                    if (initialController.text != "" &&
+                                        rateOfReturnController.text != "" &&
+                                        durationController.text != "") {
+                                      setState(() {
+                                        totalAmount = int.parse(initialController.text);
 
-                                onPressed: (){}, child: Text("Caculate")),
+                                        for(int i= 1; i <= int.parse(durationController.text); i++){
+                                          totalAmount = ((totalAmount * int.parse(rateOfReturnController.text))/ 100).round() + totalAmount;
+                                          // totalAmount = profitAmount ;
+                                        }
+
+                                        // rateOdReturn = (int.parse(rateOfReturnController.text) ;
+                                        // totalAmount = int.parse(initialController.text) * rateOdReturn;
+
+                                        print(initialAmount);
+                                        print(totalAmount);
+                                      });
+                                    }
+                                  },
+                                  child: Text("Caculate")),
+                            ),
                           ),
                         ],
-                      )
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Container(
+                            width: MediaQuery.of(context).size.width / 3,
+                            child: Text(
+                              "Invested Amount",
+                              style: TextStyle(
+                                  color: MyColor.mainColor1,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 20,
+                          ),
+                          AutoSizeText(
+                            "Rs ${initialAmount}",
+                            style: TextStyle(
+                              color: MyColor.mainColor2,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Container(
+                            width: MediaQuery.of(context).size.width / 3,
+                            child: Text(
+                              "Muturity Value",
+                              style: TextStyle(
+                                  color: MyColor.mainColor1,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 20,
+                          ),
+                          AutoSizeText(
+                            "Rs ${totalAmount}",
+                            style: TextStyle(
+                              color: MyColor.mainColor2,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Text(
+                            "Power of Compunding",
+                            style: TextStyle(
+                                color: MyColor.mainColor2.withOpacity(0.5),
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold),
+                          ),
+
+                        ],
+                      ),
+
+
                     ],
                   ),
                 )
--- lib/ui kit/ui.dart
+++ lib/ui kit/ui.dart
@@ -2,4 +2,5 @@ import 'dart:ui';
 
 class MyColor {
   static final Color mainColor1 = Color(0xFFFFB940);
+  static final Color mainColor2 = Color(0xFF2F2E41);
 }
